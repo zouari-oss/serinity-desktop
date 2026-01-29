50757259f761adc98c29e5c1e1df56d878bcf157
ZouariOmar <zouariomar20@gmail.com>
2026-01-29 12:28:32 +0100

    feat(access-control): Support `i18n` infrastructure
    [DETAILS]
    - We support `fr` & `en`
    - We make the work more dynamique, so any refactoring action will be
    easy :)

--- project/access-control/src/main/java/com/serinity/accesscontrol/App.java
+++ project/access-control/src/main/java/com/serinity/accesscontrol/App.java
@@ -1,28 +1,48 @@
+/**
+ * App.java
+ *
+ * Serinity app entry point
+ *
+ * <p>none</p>
+ *
+ * @author @ZouariOmar (zouariomar20@gmail.com)
+ * @version 1.0
+ * @since 2026-01-28
+ *
+ * <a href="https://github.com/zouari-oss/hashhash-books" target="_blank">App.java</a>
+ */
+
+// `App` package name
 package com.serinity.accesscontrol;
 
+// `serinity` imports
+import com.serinity.accesscontrol.flag.FXMLFile;
+import com.serinity.accesscontrol.util.FXMLLoaderUtil;
+import com.serinity.accesscontrol.util.I18nUtil;
+
+// `javafx` imports
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
- * JavaFX App
+ * App class entry point (launcher)
+ *
+ * <p>
+ * More detailed description or notes.
+ * </p>
  */
 public class App extends Application {
-
-  @Override
-  public void start(Stage stage) {
-    var javaVersion = SystemInfo.javaVersion();
-    var javafxVersion = SystemInfo.javafxVersion();
-
-    var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    var scene = new Scene(new StackPane(label), 640, 480);
-    stage.setScene(scene);
-    stage.show();
+  public static void main(final String[] args) {
+    launch();
   }
 
-  public static void main(String[] args) {
-    launch();
+  @Override
+  public void start(final Stage stage) {
+    I18nUtil.applySupportedLocale();
+    stage.setScene(FXMLLoaderUtil.loadScene(
+        this.getClass(),
+        FXMLFile.SIGNIN.getFileName(),
+        I18nUtil.getBundle()));
+    stage.show();
   }
 } // App class
