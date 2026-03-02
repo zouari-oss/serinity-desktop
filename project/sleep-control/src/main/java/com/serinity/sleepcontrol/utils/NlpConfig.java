32ad1b359e0c1bcbc2aca3fe4f2f25714ec12c9f
safa_jelassi <safa.jelassi@esprit.tn>
2026-03-02 06:33:42 +0100

    NLP config

---
+++ project/sleep-control/src/main/java/com/serinity/sleepcontrol/utils/NlpConfig.java
@@ -0,0 +1,9 @@
+package com.serinity.sleepcontrol.utils;
+
+public class NlpConfig {
+    public static final String HF_TOKEN = "hf_COLLE_TON_TOKEN_ICI";
+    public static final String MODEL_SENTIMENT =
+            "ac0hik/Sentiment_Analysis_French";
+    public static final String HF_URL =
+            "https://router.huggingface.co/hf-inference/models/";
+}
